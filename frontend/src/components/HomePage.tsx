6f51605853778090c37fec6a3a64758cb3726115
Omar Aizab <omaraizab98@gmail.com>
2023-09-14 16:22:22 +0100

    dev: add storybook

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -2,10 +2,10 @@ export default function HomePage() {
     return (
         <main
             className="h-screen w-screen
-            bg-points bg-cover bg-no-repeat bg-center
+            bg-points bg-cover  bg-no-repeat bg-center
             "
         >
-            Hello
+            Hellooo
         </main>
     );
 }
