f9ce0201628f9bb5c8629e193a92e8d94192b27a
Mou-SED <moseddik@student.1337.ma>
2023-09-16 20:59:46 +0100

    Chore: Add sories for PlayerCard

---
+++ frontend/src/stories/PlayerCard.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta } from '@storybook/react';
+import PlayerCard from '../components/PlayerCard';
+
+import img from './assets/1.jpeg'
+
+const meta: Meta<typeof PlayerCard> = {
+	title: 'PlayerCard',
+	component: PlayerCard,
+};
+
+export default meta;
+
+export const Default = (): JSX.Element => (
+	<>
+		<PlayerCard img={img.src} name="Tommy" />
+	</>
+);
