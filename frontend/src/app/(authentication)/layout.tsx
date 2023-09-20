352b726a2480ffece7978d422b694ad3fa066b7f
Omar Aizab <omaraizab98@gmail.com>
2023-09-20 18:59:33 +0100

    feat: add pong table to login layout

--- frontend/src/app/(authentication)/layout.tsx
+++ frontend/src/app/(authentication)/layout.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import table from './table.svg';
 
 export default function AuthLayout({
     children,
 }: {
     children: React.ReactNode;
 }): JSX.Element {
-    return <>{children}</>;
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div className="col-span-1 lg:w-1/2 lg:m-auto">{children}</div>
+            <div className="hidden lg:block overflow-hidden mt-12">
+                <img src={table.src} alt="table" />
+            </div>
+        </div>
+    );
 }
