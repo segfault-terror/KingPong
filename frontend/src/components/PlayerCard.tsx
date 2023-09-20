21ff1d70291fb9aa5d850e5484f7d04969f2e6cf
Mou-SED <moseddik@student.1337.ma>
2023-09-20 16:41:17 +0100

    fix: version mobile

--- frontend/src/components/PlayerCard.tsx
+++ frontend/src/components/PlayerCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
 
 export default function PlayerCard({
@@ -8,37 +9,29 @@ export default function PlayerCard({
     name: string;
 }) {
     return (
-        <div className="min-w-screen flex min-h-screen items-center justify-center">
             <div
-                className="h-96 w-72 rounded-2xl
-				bg-white bg-gradient-to-t from-gray-500 via-gray-300 to-white p-1"
+                className="h-48 w-36 rounded-2xl
+				bg-gradient-to-t from-gray-500 via-gray-300 to-white p-[2px]"
             >
                 <div
-                    className="flex h-full w-full flex-col
-						items-center justify-center gap-12 rounded-2xl bg-white"
+                    className="h-full w-full
+                        flex flex-col items-center justify-around
+                        rounded-2xl bg-white"
                 >
+                    <img
+                        src={img}
+                        alt="player"
+                        className="rounded-xl h-[65%] w-[90%] object-cover
+                        shadow-[0_2.8px_2.2px_rgba(37,10,59,_0.034),_0_6.7px_5.3px_rgba(37,10,59,_0.048),_0_12.5px_10px_rgba(37,10,59,_0.06),_0_22.3px_17.9px_rgba(37,10,59,_0.072),_0_41.8px_33.4px_rgba(37,10,59,_0.086),_0_100px_80px_rgba(37,10,59,_0.12)]"
+                    />
                     <div
-                        className="relative h-52 w-60 rounded-2xl bg-white
-					bg-gradient-to-t from-gray-400 via-gray-200 to-white
-					shadow-[0.0px_10.0px_12.0px_rgba(37,10,59,0.85)]"
-                    >
-                        <Image
-                            src={img}
-                            alt="player"
-                            fill={true}
-                            className="rounded-2xl
-						object-cover"
-                        />
-                    </div>
-                    <div
-                        className="bg-secondary-200 font-nicomoji grid h-12 w-60 items-center
-						justify-center rounded-2xl text-2xl
-						shadow-[0.0px_10.0px_12.0px_rgba(37,10,59,0.80)]"
+                        className="w-[90%] bg-secondary-200 font-nicomoji grid items-center
+						justify-center rounded-2xl text-sm text-primary
+                        shadow-[0_2.8px_2.2px_rgba(37,10,59,_0.034),_0_6.7px_5.3px_rgba(37,10,59,_0.048),_0_12.5px_10px_rgba(37,10,59,_0.06),_0_22.3px_17.9px_rgba(37,10,59,_0.072),_0_41.8px_33.4px_rgba(37,10,59,_0.086),_0_100px_80px_rgba(37,10,59,_0.12)]"
                     >
                         {name}
                     </div>
                 </div>
             </div>
-        </div>
     );
 }
