2fac5e9ad0417a5b80e63060efd6e4867789e371
Mou-SED <moseddik@student.1337.ma>
2023-09-23 11:46:09 +0100

    style(ui/Matchmaking): make text large for Desktop

--- frontend/src/app/(game)/matchmaking/PlayerCard.tsx
+++ frontend/src/app/(game)/matchmaking/PlayerCard.tsx
@@ -26,7 +26,7 @@ export default function PlayerCard({
                 />
                 <div
                     className="w-[90%] bg-secondary-200 font-nicomoji grid items-center
-						justify-center rounded-2xl text-sm text-primary
+						justify-center rounded-2xl text-sm lg:text-lg text-primary
                         shadow-[0_2.8px_2.2px_rgba(37,10,59,_0.034),_0_6.7px_5.3px_rgba(37,10,59,_0.048),_0_12.5px_10px_rgba(37,10,59,_0.06),_0_22.3px_17.9px_rgba(37,10,59,_0.072),_0_41.8px_33.4px_rgba(37,10,59,_0.086),_0_100px_80px_rgba(37,10,59,_0.12)]"
                 >
                     {name}
