8431ad9df0d8ab791621733c46f7363c275e679e
Mou-SED <moseddik@student.1337.ma>
2023-09-24 15:31:22 +0100

    feat(ui/UserCircleInfo): add props

---
+++ frontend/src/stories/UserCircleInfo.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react';
+import UserCircleInfo from '../../src/app/profile/UserCircleInfo';
+
+import Avatar from './assets/3.jpeg';
+import { UserStatus } from '../components/DirectMessage';
+
+const meta: Meta<typeof UserCircleInfo> = {
+    title: 'UserCircleInfo',
+    component: UserCircleInfo,
+};
+
+export default meta;
+type Story = StoryObj<typeof UserCircleInfo>;
+
+export const Primary: Story = {
+    args: {
+        avatarPath: Avatar.src,
+        level: 100,
+        status: UserStatus.Online,
+    },
+};
