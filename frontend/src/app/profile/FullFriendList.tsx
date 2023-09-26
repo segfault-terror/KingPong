2cd0df4e7865ab5c3e937d3c2a395c3658c8cef9
Hamza Haddani <hhamza@student.1337.ma>
2023-09-26 11:30:44 +0100

    fix(ui/FullFriendList): Space before button
    Remove spacing before the 'Full Friend List' button

--- frontend/src/app/profile/FullFriendList.tsx
+++ frontend/src/app/profile/FullFriendList.tsx
@@ -1,41 +1,33 @@
 import UserCircleInfo, { UserCircleInfoProps } from './UserCircleInfo';
-import { UserStatus } from '../../components/DirectMessage';
-
-import Tommy from '../../stories/assets/1.jpeg';
 
 type FullFriendListProps = {
-	lastFriends: UserCircleInfoProps[];
+    lastFriends: UserCircleInfoProps[];
 };
 
-export default function FullFriendList( {lastFriends}: FullFriendListProps ) {
-	if (lastFriends.length > 3) {
-		throw new Error('FullFriendList can only display 3 friends');
-	}
+export default function FullFriendList({ lastFriends }: FullFriendListProps) {
+    if (lastFriends.length > 3) {
+        throw new Error('FullFriendList can only display 3 friends');
+    }
 
     return (
-        <>
+        <div>
             <div
                 className="bg-primary bg-opacity-80 rounded-t-2xl
-			flex justify-evenly py-4"
+                    flex justify-evenly py-4"
             >
-            {lastFriends.map((friend, idx) => {
-				return (
-					<UserCircleInfo
-						key={idx}
-						{...friend}
-					/>
-				);
-			})}
+                {lastFriends.map((friend, idx) => {
+                    return <UserCircleInfo key={idx} {...friend} />;
+                })}
             </div>
             <div
                 className="flex items-center justify-center
-				text-sm text-white
-				bg-gradient-to-t from-[#881EDF] to-secondary-200
-				w-full h-8
-				rounded-b-2xl"
+                    text-sm text-white
+                    bg-gradient-to-t from-[#881EDF] to-secondary-200
+                    w-full h-8
+                    rounded-b-2xl"
             >
                 Full Friend List
             </div>
-        </>
+        </div>
     );
 }
