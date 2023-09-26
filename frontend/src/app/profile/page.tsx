f17054594f733e965b67537d6c9b6830de14a969
Hamza Haddani <hhamza@student.1337.ma>
2023-09-26 11:43:18 +0100

    feat(ui/profile): Add profile page with dummy data

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,12 +1,51 @@
-import Logo from '../../components/Logo';
-import { AiOutlineBell } from 'react-icons/ai';
-import UserCircleInfo from './UserCircleInfo';
-
-import Archer from '../../stories/assets/2.jpeg';
 import { UserStatus } from '@/components/DirectMessage';
+import { AiOutlineBell } from 'react-icons/ai';
+import AchievementGold from '../../../public/images/gold-achievement.svg';
+import Logo from '../../components/Logo';
+import Tommy from '../../stories/assets/1.jpeg';
+import Archer from '../../stories/assets/2.jpeg';
+import Moussa from '../../stories/assets/3.jpeg';
+import AchievementList from './AchievementList';
+import FullFriendList from './FullFriendList';
 import MatchHistory from './MatchHistory';
+import ProfileCard from './ProfileCard';
+import GoldLeague from '../../../public/images/gold-league.svg';
 
 export default function ProfilePage() {
+    const achievements = [
+        {
+            title: 'First Win',
+            description: 'Win your first game',
+            image: AchievementGold.src,
+        },
+        {
+            title: 'First Win',
+            description: 'Win your first game',
+            image: AchievementGold.src,
+        },
+        {
+            title: 'First Win',
+            description: 'Win your first game',
+            image: AchievementGold.src,
+        },
+    ];
+    const lastFriends = [
+        {
+            avatarPath: Archer.src,
+            level: 99,
+            status: UserStatus.Online,
+        },
+        {
+            avatarPath: Tommy.src,
+            level: 72,
+            status: UserStatus.Online,
+        },
+        {
+            avatarPath: Moussa.src,
+            level: 64,
+            status: UserStatus.Online,
+        },
+    ];
     const matchHistory = [
         {
             playerAvatar: Archer.src,
@@ -40,12 +79,20 @@ export default function ProfilePage() {
                 <Logo mylogo="/images/logo.svg" className="w-1/2" />
                 <AiOutlineBell className="text-secondary-200 text-3xl" />
             </div>
-            <UserCircleInfo
-                avatarPath={Archer.src}
-                level={100}
-                status={UserStatus.Online}
-            />
-            <MatchHistory gameResults={matchHistory} />
+            <div className="flex flex-col gap-2 mx-4 mt-12 mb-8">
+                <ProfileCard
+                    userName="Archer"
+                    avatarPath={Archer.src}
+                    level={99}
+                    status={UserStatus.Online}
+                    league={GoldLeague.src}
+                    wins={100}
+                    losses={0}
+                />
+                <AchievementList achievements={achievements} />
+                <FullFriendList lastFriends={lastFriends} />
+                <MatchHistory gameResults={matchHistory} />
+            </div>
         </>
     );
 }
