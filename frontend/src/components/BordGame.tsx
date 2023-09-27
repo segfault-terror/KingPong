e6033d03826b64c62dc00cb118655916b5f96bca
AymanAkashi <aaggoujj@student.1337.ma>
2023-09-27 13:22:21 +0100

    feat: add some animation in home page

--- frontend/src/components/BordGame.tsx
+++ frontend/src/components/BordGame.tsx
@@ -2,7 +2,7 @@ import Logo from '@/components/Logo';
 
 export default function BordGame() {
     return (
-        <div className="border-secondary-500 flex h-[100px] w-[200px] items-center justify-between rounded-3xl border-8 sm:h-[250px] sm:w-[350px] md:h-[380px] md:w-[620px] lg:h-[500px] lg:w-[1000px] xl:h-[600px] xl:w-[1200px] animate-border">
+        <div className="border-secondary-500 flex h-[150px] w-[300px] items-center justify-between rounded-3xl border-8 sm:h-[300px] sm:w-[370px] md:h-[380px] md:w-[620px] lg:h-[500px] lg:w-[1000px] xl:h-[600px] xl:w-[1200px] animate-border">
             <div className="from-secondary-200 to-secondary-500 sm:h-22 md:h-26 border-inactive-200 h-20 w-2 rounded-l-3xl border-2 bg-gradient-to-r sm:w-3 md:w-3  lg:h-32 lg:w-4 xl:h-36 xl:w-4"></div>
             <Logo
                 className=" animate-ball absolute h-3 w-3 sm:h-3 sm:w-3 md:h-4 md:w-4 lg:h-6 lg:w-6"
