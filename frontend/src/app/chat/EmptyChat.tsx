b97ee740bcb2dd13a8e0326892a6b63f702dc478
Hamza Haddani <hhamza@student.1337.ma>
2023-10-02 18:06:03 +0100

    feat(ui/EmptyChat): Add empty channel view

--- frontend/src/app/chat/EmptyChat.tsx
+++ frontend/src/app/chat/EmptyChat.tsx
@@ -1,6 +1,7 @@
 import { BiMessageAdd } from 'react-icons/bi';
+import { MdOutlineExplore } from 'react-icons/md';
 
-export default function EmptyChat() {
+function EmptyDM() {
     return (
         <div
             className="grid justify-center align-center
@@ -24,4 +25,49 @@ export default function EmptyChat() {
             </button>
         </div>
     );
+}
+
+function EmptyChannels() {
+    return (
+        <div
+            className="grid justify-center align-center
+                        text-center text-cube_palette-200"
+        >
+            <MdOutlineExplore className="w-20 h-20 mx-auto" />
+            <h1 className="text-2xl">Your channels</h1>
+            <p className="text-xs font-light mb-3">
+                Create or join a new channel
+            </p>
+            <button
+                onClick={() => console.log('Create channel')}
+                className="text-primary bg-secondary-200
+                            text-xs font-bold font-jost
+                            rounded-xl
+                            py-2
+                            inline-block w-32
+                            mx-auto mb-2"
+            >
+                Create channel
+            </button>
+            <button
+                onClick={() => console.log('Join channel')}
+                className="text-primary bg-secondary-200
+                            text-xs font-bold font-jost
+                            rounded-xl
+                            py-2
+                            inline-block w-32
+                            mx-auto"
+            >
+                Join channel
+            </button>
+        </div>
+    );
+}
+
+type EmptyChatProps = {
+    toggle: boolean;
+};
+
+export default function EmptyChat({ toggle }: EmptyChatProps) {
+    return toggle ? <EmptyChannels /> : <EmptyDM />;
 }
