805f47716f1efbdc53133440f07be376e2f64115
Omar Aizab <omaraizab98@gmail.com>
2023-10-03 17:52:58 +0100

    feat: hadchi li bghaw drari

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 export default function NotFound() {
     return (
         <>
-            <Header />
             <div className="flex flex-col items-center gap-8 p-8 lg:mt-52">
                 <h2 className="text-4xl font-jost font-bold">Whoops!</h2>
                 <p className="text-lg">404 Page Not Found</p>
