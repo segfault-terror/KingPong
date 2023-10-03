e0295adc065595df906a644499c79537d329a20f
Omar Aizab <omaraizab98@gmail.com>
2023-10-03 18:00:41 +0100

    feat: add brand to 404 page

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -4,6 +4,17 @@ import Link from 'next/link';
 export default function NotFound() {
     return (
         <>
+            <header className="p-3 w-full">
+                <div className="grid grid-cols-1 md:grid-cols-3 items-center">
+                    <Link href="/" className="">
+                        <img
+                            src="/images/logo.svg"
+                            className="w-56 h-auto md:w-56"
+                            alt="logo"
+                        />
+                    </Link>
+                </div>
+            </header>
             <div className="flex flex-col items-center gap-8 p-8 lg:mt-52">
                 <h2 className="text-4xl font-jost font-bold">Whoops!</h2>
                 <p className="text-lg">404 Page Not Found</p>
