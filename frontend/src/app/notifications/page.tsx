2752f3dcc33aa075c8acec07596f243fd62b5dd2
AymanAkashi <aaggoujj@student.1337.ma>
2023-10-06 16:28:56 +0100

    feat: add notifications route

---
+++ frontend/src/app/notifications/page.tsx
@@ -0,0 +1,103 @@
+import Notification from '@/components/Notifications';
+
+const NotificationState = {
+    notification: [
+        {
+            id: 1,
+            type: 'Game' as const,
+            readed: false,
+            sender: {
+                name: 'John Doe',
+                image: 'https://robohash.org/10',
+            },
+        },
+        {
+            id: 2,
+            type: 'friend' as const,
+            readed: false,
+            sender: {
+                name: 'John Doe',
+                image: 'https://robohash.org/30',
+            },
+        },
+        {
+            id: 3,
+            type: 'Game' as const,
+            readed: false,
+            sender: {
+                name: 'John Doe',
+                image: 'https://robohash.org/20',
+            },
+        },
+        {
+            id: 4,
+            type: 'friend' as const,
+            readed: true,
+            sender: {
+                name: 'John Doe',
+                image: 'https://robohash.org/40',
+            },
+        },
+        {
+            id: 5,
+            type: 'Game' as const,
+            readed: true,
+            sender: {
+                name: 'John Doe',
+                image: 'https://robohash.org/50',
+            },
+        },
+        {
+            id: 6,
+            type: 'friend' as const,
+            readed: false,
+            sender: {
+                name: 'John Doe',
+                image: 'https://robohash.org/60',
+            },
+        },
+        {
+            id: 7,
+            type: 'Game' as const,
+            readed: true,
+            sender: {
+                name: 'John Doe',
+                image: 'https://robohash.org/70',
+            },
+        },
+        {
+            id: 8,
+            type: 'friend' as const,
+            readed: true,
+            sender: {
+                name: 'John Doe',
+                image: 'https://robohash.org/80',
+            },
+        },
+        {
+            id: 9,
+            type: 'Game' as const,
+            readed: true,
+            sender: {
+                name: 'John Doe',
+                image: 'https://robohash.org/90',
+            },
+        },
+        {
+            id: 10,
+            type: 'friend' as const,
+            readed: true,
+            sender: {
+                name: 'John Doe',
+                image: 'https://robohash.org/100',
+            },
+        }
+    ],
+  };
+
+  const Empty = []
+
+
+export default function Page(){
+	return <Notification notification={[]} />
+}
--- frontend/src/components/Notifications.tsx
+++ frontend/src/components/Notifications.tsx
@@ -1,13 +1,16 @@
+'use client'
+
 import Header from '@/app/(dashboard)/Header';
 import Image from 'next/image';
+import Ghost from '@/components/Ghost';
+
 
 import Decline from '@/../public/images/decline.svg';
+import { read } from 'fs';
+
 const Cross = () => {
-    return (
-        <Image src={Decline} alt="Decline">
-        </Image>
-    )
-}
+    return;
+};
 
 type User = {
     name: string;
@@ -15,7 +18,9 @@ type User = {
 };
 
 type NotificationProps = {
+    id: number;
     type: 'Game' | 'friend';
+    readed: boolean;
     sender: User;
 };
 
@@ -23,54 +28,65 @@ type NotificationState = {
     notification: NotificationProps[];
 };
 
-const Notife = (type: string, sender: string) => {
-    if (type == 'Game')
-        return (
-            <div className="flex justify-start items-center h-24 w-full my-1 bg-black">
-                <div className="font-jost text-white">
-                    {`${sender} has invited you to a game!`}
-                    <img src="/images/fight.svg" alt="fight" className=''  />
-                </div>
-            </div>
-        );
-    else if (type == 'friend') {
-        return (
-            <div className="flex justify-start items-center h-24 w-full my-1 bg-black">
-                <p className="font-jost text-white">
-                    {`${sender} has sent you a friend request!`}
-                </p>
-            </div>
-        );
-    }
+const Notife = (type: string, sender: User, readed: boolean = false) => {
+    const message =
+        type == 'Game'
+            ? `${sender.name} has invited you to a game!`
+            : `${sender.name} has sent you a friend request!`;
+    const image =
+        type == 'Game' ? '/images/fight.svg' : '/images/add-friend.svg';
+    const color = readed == true ? 'bg-background' : 'bg-amber-500';
+    return (
+        <div
+            className={`flex justify-between items-center h-24 w-full my-1 ${color} rounded-2xl`}
+        >
+            <img
+                src={sender.image}
+                alt="image"
+                className="w-20 rounded-full border-white border m-2"
+            />
+            <div className="font-jost text-white">{message}</div>
+            <img src={image} alt="fight" className="w-12 px-1" />
+            <button className="px-2">
+                <Image src={Decline} alt="Decline"></Image>
+            </button>
+        </div>
+    );
+};
+
+const Empty = () => {
+    return (
+        <div className="relative flex justify-center items-center h-24 w-full my-1 bg-background rounded-2xl">
+            <div className=" absolute inset-0 flex flex-col justify-center items-center font-jost text-white">No new notifications</div>
+            <Ghost className="w-[30%] md:w-[20%] drop-shadow-neon-white" />
+        </div>
+    );
 };
 
 export default function Notification({ notification }: NotificationState) {
     return (
-        <div className="min-h-screen">
+        <>
             <Header />
-            <div className="mt-2 flex flex-col justify-center items-center content-center">
-                {notification.length > 0 ? (
-                    <>
-                        {notification.map((notif) => (
-                            <div
-                                key={`${notif.type}-${notif.sender.name}`}
-                                className="flex justify-around items-center h-24 w-full my-1 bg-black"
-                            >
-                                <div className="h-24 w-24 bg-slate-400 rounded-full mx-1">
-                                    <img
-                                        src={notif.sender.image}
-                                        alt={notif.sender.name}
-                                    />
-                                </div>
-                                <div>{Notife(notif.type, notif.sender.name)}</div>
-                                <button className='sm:w-4'>{Cross()}</button>
-                            </div>
-                        ))}
-                    </>
-                ) : (
-                    <p>No notifications to display.</p>
-                )}
+            <div className="flex justify-center items-center w-full">
+                {/* <h1 className="font-jost text-3xl text-white">Notifications</h1> */}
             </div>
-        </div>
+            <div className="felx flex-col justify-center items-center w-full p-3 md:p-6">
+                <div className="flex justify-center items-center w-full lg:max-w-3xl lg:mx-auto pl-4 py-2 px-3 bg-primary border rounded-lg border-secondary-500 drop-shadow-neon-orange">
+                    <div className="flex flex-col justify-between items-center w-full lg:px-3">
+                        {notification.length == 0
+                            ? Empty() 
+                            : notification.map((notif) => (
+                                  <div className="w-full flex justify-between items-center my-1 rounded-xl">
+                                      {Notife(
+                                          notif.type,
+                                          notif.sender,
+                                          notif.readed,
+                                      )}
+                                  </div>
+                              ))}
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
