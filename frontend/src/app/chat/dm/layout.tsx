91fe95d1aa7c37f60c3372c0ba1e3de971fc3025
Mou-SED <moseddik@student.1337.ma>
2023-10-07 21:54:42 +0100

    wip(ui/chat): Desktop layout

---
+++ frontend/src/app/chat/dm/layout.tsx
@@ -0,0 +1,33 @@
+'use client';
+import ChatSideBar from '@/app/chat/ChatSideBar';
+import { DMList } from '@/app/chat/data/ChatData';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
+import React, { useEffect, useState } from 'react';
+
+export default function DMLayout({ children }: { children: React.ReactNode }) {
+    const matches = useMediaQuery('(min-width: 1024px)');
+    const [isRendred, setIsRendred] = useState<boolean>(false);
+
+    useEffect(() => {
+        setIsRendred(true);
+    }, []);
+
+    if (!isRendred) {
+        return null;
+    }
+
+    if (matches) {
+        return (
+            <div>
+                <ChatSideBar
+                    messagesList={DMList}
+                    channelList={[]}
+                    toggle={false}
+                    setToggle={(toggle) => toggle}
+                />
+                {children}
+            </div>
+        );
+    }
+    return children;
+}
