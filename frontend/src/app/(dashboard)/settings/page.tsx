446abc98debaac9358f0f0dc2febe3e2b699db43
Omar Aizab <omaraizab98@gmail.com>
2023-10-07 14:22:55 +0100

    feat: create upload image component

--- frontend/src/app/(dashboard)/settings/page.tsx
+++ frontend/src/app/(dashboard)/settings/page.tsx
@@ -3,14 +3,24 @@
 import Image from 'next/image';
 import React from 'react';
 
-function UploadInput() {
+const profile = {
+    fullname: 'John Doe',
+    usernamr: 'johndoe',
+    avatar: '/images/4.jpeg',
+};
+
+function UploadInput({ setImage }: { setImage: (image: string) => void }) {
     const inputRef = React.useRef<HTMLInputElement>(null);
     const [fileSelected, setFileSelected] = React.useState(false);
 
     return (
         <>
-            <button className="" onClick={() => inputRef.current?.click()}>
-                open
+            <button
+                className="font-jost font-bold text-sm text-secondary-200 border border-secondary-200 rounded-3xl
+                            px-4 py-2 bg-primary hover:bg-secondary-200 hover:text-background transition-all"
+                onClick={() => inputRef.current?.click()}
+            >
+                Upload Avatar
             </button>
             {fileSelected && (
                 <button
@@ -18,6 +28,7 @@ function UploadInput() {
                         if (!inputRef.current) return;
                         inputRef.current.value = '';
                         setFileSelected(inputRef.current?.files?.length === 1);
+                        setImage(profile.avatar);
                     }}
                 >
                     {inputRef.current?.files?.[0].name}
@@ -26,21 +37,24 @@ function UploadInput() {
             <input
                 onChange={() => {
                     setFileSelected(inputRef.current?.files?.length === 1);
+                    if (!inputRef.current) return;
+                    const file = inputRef.current.files?.[0];
+                    if (!file) return;
+                    const fileUrl = URL.createObjectURL(file);
+                    setImage(fileUrl);
+                    console.log(fileUrl);
                 }}
                 type="file"
                 className="hidden"
                 ref={inputRef}
+                accept="image/*"
             />
         </>
     );
 }
 
 export default function Settings() {
-    const profile = {
-        fullname: 'John Doe',
-        usernamr: 'johndoe',
-        avatar: '/images/4.jpeg',
-    };
+    const [image, setImage] = React.useState(profile.avatar);
     return (
         <main className="flex items-center justify-center my-9">
             <div
@@ -51,15 +65,15 @@ export default function Settings() {
                     Settings
                 </h2>
                 <div>
-                    <div className="flex flex-col items-center justify-center">
+                    <div className="flex flex-col items-center justify-center gap-4">
                         <Image
-                            src={profile.avatar}
+                            src={image}
                             alt="avatar"
                             width={128}
                             height={128}
                             className="w-32 h-32 object-cover rounded-full border border-secondary-200"
                         />
-                        <UploadInput />
+                        <UploadInput setImage={setImage} />
                     </div>
                     <div></div>
                 </div>
