54f958cb9b39637d9ad105688d976ff52849a75b
Omar Aizab <omaraizab98@gmail.com>
2023-10-08 21:45:20 +0100

    feat: add CRUD operation to user service

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -13,4 +13,44 @@ export class UserService {
             where: userWhereUniqueInput,
         });
     }
+
+    async users(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.UserWhereUniqueInput;
+        where?: Prisma.UserWhereInput;
+        orderBy?: Prisma.UserOrderByWithRelationInput;
+    }): Promise<User[]> {
+        const { skip, take, cursor, where, orderBy } = params;
+        return this.prisma.user.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+        });
+    }
+
+    async createUser(data: Prisma.UserCreateInput): Promise<User> {
+        return this.prisma.user.create({
+            data,
+        });
+    }
+
+    async updateUser(params: {
+        where: Prisma.UserWhereUniqueInput;
+        data: Prisma.UserUpdateInput;
+    }): Promise<User> {
+        const { where, data } = params;
+        return this.prisma.user.update({
+            data,
+            where,
+        });
+    }
+
+    async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+        return this.prisma.user.delete({
+            where,
+        });
+    }
 }
