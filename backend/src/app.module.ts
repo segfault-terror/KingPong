85eca86677c08fb359b5020cc3434f171e052a0a
Omar Aizab <omaraizab98@gmail.com>
2023-10-09 15:54:45 +0100

    feat: create and configure session store

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'nestjs-prisma';
 import { prismaModuleOptions } from './modules.options';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         PrismaModule.forRoot(prismaModuleOptions),
+        PassportModule.register({ session: true }),
     ],
     controllers: [AppController],
     providers: [AppService],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,39 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+import { ConfigService } from '@nestjs/config';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import { PrismaClient } from '@prisma/client';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     const { httpAdapter } = app.get(HttpAdapterHost);
     app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+    const configService = app.get(ConfigService);
+    const secret = configService.get<string>('SESSION_SECRET');
+
+    app.use(
+        session({
+            secret,
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 15,
+            },
+            store: new PrismaSessionStore(new PrismaClient(), {
+                checkPeriod: 2 * 60 * 1000, //ms
+                dbRecordIdIsSessionId: true,
+                dbRecordIdFunction: undefined,
+            }),
+        }),
+    );
+
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     await app.listen(3000);
 }
