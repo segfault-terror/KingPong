7bdf1189b0afb5169f8c58a3c957620eae45d8f4
Omar Aizab <omaraizab98@gmail.com>
2023-10-10 15:47:42 +0100

    chore: clean user checking

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -4,9 +4,10 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 export class AuthGard implements CanActivate {
     canActivate(context: ExecutionContext): boolean | Promise<boolean> {
         const request = context.switchToHttp().getRequest();
-        if (request.user) {
-            return true;
-        }
-        return false;
+        // if (request.user) {
+        //     return true;
+        // }
+        // return false;
+        return request.isAuthenticated();
     }
 }
