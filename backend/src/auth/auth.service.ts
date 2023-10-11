9be4dc3a5bd9451bde486f2be3bdd1d2f4a3b3b2
Omar Aizab <omaraizab98@gmail.com>
2023-10-11 18:05:45 +0100

    fix: duplicate username in oauth2

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -6,11 +6,23 @@ import { UserService } from 'src/user/user.service';
 export class AuthService {
     constructor(private readonly userService: UserService) {}
 
+    async checkUsername(details: Prisma.UserCreateInput): Promise<boolean> {
+        const user = await this.userService.user({
+            username: details.username,
+        });
+
+        return user ? true : false;
+    }
+
     async validateUserOAuth2(details: Prisma.UserCreateInput): Promise<any> {
         const user = await this.userService.user({ email: details.email });
 
         if (user) {
             return user;
+        }
+
+        while (await this.checkUsername(details)) {
+            details.username += Math.floor(Math.random() * 10);
         }
         return await this.userService.createUser(details);
     }
