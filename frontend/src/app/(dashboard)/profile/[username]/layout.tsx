52a666a998c65db9657be8b7ae005cc742aa09cf
Mou-SED <moseddik@student.1337.ma>
2023-10-14 17:35:20 +0100

    feat(ui/profile): add achivementModal

--- frontend/src/app/(dashboard)/profile/[username]/layout.tsx
+++ frontend/src/app/(dashboard)/profile/[username]/layout.tsx
@@ -1,6 +1,9 @@
 'use client';
 import Modal from '@/app/chat/Modal';
 import { createContext, useState } from 'react';
+import AchievementModal from '../AchievementModal';
+import { usePathname } from 'next/navigation';
+import path from 'path';
 
 type ProfileLayoutProps = {
     children: React.ReactNode;
@@ -23,21 +26,35 @@ export default function ProfileLayout({ children }: ProfileLayoutProps) {
     const [achievements, setAchievements] = useState(false);
     const [friends, setFriends] = useState(false);
     const [matches, setMatches] = useState(false);
+    const pathname = usePathname();
 
     return (
         <>
             {achievements && (
-                <Modal onClose={() => setAchievements(false)}>
-                    <p>achievement list modal</p>
+                <Modal
+                    onClose={() => setAchievements(false)}
+                    childrenClassName="bg-background p-6 rounded-2xl border-2 border-white w-[90%]
+                                        lg:w-2/3 max-w-[600px] max-h-[635px]
+                                        overflow-y-auto"
+                >
+                    <AchievementModal userName={path.basename(pathname)} />
                 </Modal>
             )}
             {friends && (
-                <Modal onClose={() => setFriends(false)}>
+                <Modal
+                    onClose={() => setFriends(false)}
+                    childrenClassName="bg-background p-6 rounded-2xl border-2 border-white w-[90%]
+                                        lg:w-2/3 max-w-[600px]"
+                >
                     <p>friends list modal</p>
                 </Modal>
             )}
             {matches && (
-                <Modal onClose={() => setMatches(false)}>
+                <Modal
+                    onClose={() => setMatches(false)}
+                    childrenClassName="bg-background p-6 rounded-2xl border-2 border-white w-[90%]
+                                        lg:w-2/3 max-w-[600px]"
+                >
                     <p>matches list modal</p>
                 </Modal>
             )}
