44fa9143254ae3c76d19775f3385d80e071323a5
Omar Aizab <omaraizab98@gmail.com>
2023-10-15 19:16:06 +0100

    fix: make queryKey unique

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
     children: React.ReactNode;
 }) {
     const { error, data, isLoading } = useQuery({
-        queryKey: ['auth'],
+        queryKey: ['authDashboard'],
         queryFn: async () => {
             try {
                 return await axios.get('http://localhost:3000/auth/status', {
