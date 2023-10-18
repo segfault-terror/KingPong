07a0f0cea6bcbe05f9c7b43c7ad8d34a8e1f2df9
AymanAkashi <aaggoujj@student.1337.ma>
2023-10-18 19:42:09 +0100

    feat: fetch data notification from db

--- frontend/src/app/(dashboard)/notifications/page.tsx
+++ frontend/src/app/(dashboard)/notifications/page.tsx
@@ -1,107 +1,39 @@
+'use client'
+
 import Notification from './Notifications';
 import { NotificationProps, NotificationState } from './types';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '@/app/loading';
+import { useContext } from 'react';
 
-const Notifications = {
-    notification: [
-        {
-            id: 1,
-            type: 'Game' as const,
-            readed: false,
-            sender: {
-                name: 'John Doe',
-                avatar: 'https://robohash.org/10',
-            },
-        },
-        {
-            id: 2,
-            type: 'Friend' as const,
-            readed: false,
-            sender: {
-                name: 'John Doe',
-                avatar: 'https://robohash.org/30',
-            },
-        },
-        {
-            id: 3,
-            type: 'Game' as const,
-            readed: false,
-            sender: {
-                name: 'John Doe',
-                avatar: 'https://robohash.org/20',
-            },
-        },
-        {
-            id: 4,
-            type: 'Friend' as const,
-            readed: true,
-            sender: {
-                name: 'John Doe',
-                avatar: 'https://robohash.org/40',
-            },
-        },
-        {
-            id: 5,
-            type: 'Game' as const,
-            readed: true,
-            sender: {
-                name: 'John Doe',
-                avatar: 'https://robohash.org/50',
-            },
-        },
-        {
-            id: 6,
-            type: 'Friend' as const,
-            readed: false,
-            sender: {
-                name: 'John Doe',
-                avatar: 'https://robohash.org/60',
-            },
-        },
-        {
-            id: 7,
-            type: 'Game' as const,
-            readed: true,
-            sender: {
-                name: 'John Doe',
-                avatar: 'https://robohash.org/70',
-            },
-        },
-        {
-            id: 8,
-            type: 'Friend' as const,
-            readed: true,
-            sender: {
-                name: 'John Doe',
-                avatar: 'https://robohash.org/80',
-            },
-        },
-        {
-            id: 9,
-            type: 'Game' as const,
-            readed: true,
-            sender: {
-                name: 'John Doe',
-                avatar: 'https://robohash.org/90',
-            },
-        },
-        {
-            id: 10,
-            type: 'Friend' as const,
-            readed: true,
-            sender: {
-                name: 'John Doe',
-                avatar: 'https://robohash.org/100',
-            },
-        },
-    ],
-};
-
-const Empty = [];
 
 export default function Page() {
+    const { data, isLoading } = useQuery({
+        queryKey: ['notifications'],
+        queryFn: async () => {
+            const { data } = await axios.get(
+                `http://localhost:3000/notifications`,
+                {
+                    withCredentials: true,
+                },
+            );
+            return data;
+        },
+    });
+    if (isLoading) {
+        return <Loading />;
+    }
+    const notifications = data.map((notification: any) => ({
+        username: notification.user.username,
+        avatar: notification.user.avatar,
+        readed: notification.readed,
+        type: notification.type,
+    }));
+
     return (
         <div id="Notification">
-            <Notification notifications={Notifications.notification} />
+            <Notification notifications={notifications} />
         </div>
     );
 }
