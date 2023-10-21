e27734d5b4f36c44e5159d16b325f2ab1da530bb
Mou-SED <moseddik@student.1337.ma>
2023-10-21 11:58:11 +0100

    feat(ui/profile): Link Friend list to /friends
    - Remove Friend list component
    - Link to /friends route

--- frontend/src/app/(dashboard)/profile/FullFriendList.tsx
+++ frontend/src/app/(dashboard)/profile/FullFriendList.tsx
@@ -4,8 +4,6 @@ import Ghost from '../../../../public/lottie/ghost.json';
 import UserCircleInfo from './UserCircleInfo';
 import { UsersFriends } from './data/ProfileData';
 import Link from 'next/link';
-import { useContext } from 'react';
-import { profileModalContext } from '@/contexts/contexts';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { backendHost } from '@/app/globals';
@@ -25,8 +23,6 @@ export default function FullFriendList({ username }: FullFriendListProps) {
             return user.data;
         },
     });
-
-    const { setFriends } = useContext(profileModalContext);
 
     if (isLoading) return <div>Loading...</div>;
     const slicedFriends = user?.friends.slice(0, 3);
@@ -71,16 +67,16 @@ export default function FullFriendList({ username }: FullFriendListProps) {
             </div>
 
             {user?.friends.length > 3 && (
-                <button
+                <Link
+                href={`/friends`}
                     className="flex items-center justify-center
                     text-sm text-white
                     bg-gradient-to-t from-[#881EDF] to-secondary-200
                     w-full h-8
                     rounded-b-2xl"
-                    onClick={() => setFriends(true)}
                 >
                     Full Friend List
-                </button>
+                </Link>
             )}
         </div>
     );
--- frontend/src/app/(dashboard)/profile/[username]/layout.tsx
+++ frontend/src/app/(dashboard)/profile/[username]/layout.tsx
@@ -5,7 +5,6 @@ import { usePathname } from 'next/navigation';
 import path from 'path';
 import { useState } from 'react';
 import AchievementModal from '../AchievementModal';
-import FriendListModal from '../FriendListModal';
 import GameResultModal from '../MatchHistoryModal';
 
 type ProfileLayoutProps = {
@@ -14,7 +13,6 @@ type ProfileLayoutProps = {
 
 export default function ProfileLayout({ children }: ProfileLayoutProps) {
     const [achievements, setAchievements] = useState(false);
-    const [friends, setFriends] = useState(false);
     const [matches, setMatches] = useState(false);
     const pathname = usePathname();
 
@@ -29,17 +27,6 @@ export default function ProfileLayout({ children }: ProfileLayoutProps) {
                                         scrollbar-thumb-secondary-200 scrollbar-thin"
                 >
                     <AchievementModal userName={path.basename(pathname)} />
-                </Modal>
-            )}
-            {friends && (
-                <Modal
-                    onClose={() => setFriends(false)}
-                    childrenClassName="bg-background p-6 border-2 border-white w-[90%]
-                                        lg:w-2/3 max-w-[400px] max-h-[635px]
-                                        overflow-y-auto
-                                        scrollbar-thumb-secondary-200 scrollbar-thin"
-                >
-                    <FriendListModal userName={path.basename(pathname)} />
                 </Modal>
             )}
             {matches && (
@@ -57,8 +44,6 @@ export default function ProfileLayout({ children }: ProfileLayoutProps) {
                 value={{
                     achievements,
                     setAchievements,
-                    friends,
-                    setFriends,
                     matches,
                     setMatches,
                 }}
