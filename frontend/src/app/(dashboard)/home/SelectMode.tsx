b7c3153044552ec16c20cb87e8547b6ffb7c2bac
AymanAkashi <aaggoujj@student.1337.ma>
2023-10-21 22:56:47 +0100

    feat(dashboard): add assets Home

---
+++ frontend/src/app/(dashboard)/home/SelectMode.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+
+type SelectModeProps = {
+    mode: string;
+    images: string;
+    link: string;
+    title: string;
+};
+
+export default function SelectMode({
+    mode,
+    images,
+    link,
+    title,
+}: SelectModeProps) {
+    return (
+        <div className="w-full h-72 md:h-96  flex flex-col justify-around items-center group">
+            <Link
+                href={link}
+                className="w-full rounded-xl flex justify-center items-center opacity-80 group-hover:opacity-100 hover:drop-shadow-neon-white"
+            >
+                <img
+                    src={`/images/${images}.svg`}
+                    alt=""
+                    className="w-[80%] xl:w-[600px]"
+                />
+            </Link>
+            <Link
+                href={link}
+                type="Link"
+                title={title}
+                className="flex items-center justify-center w-32 h-12 lg:w-36 lg:h-14 border-t-4 border-b-4 rounded-3xl border-opponent bg-secondary-500 text-black font-nicomoji group-hover:opacity-100  hover:bg-secondary-200 hover:border-white "
+            >
+                {mode}
+            </Link>
+        </div>
+    );
+}
--- frontend/src/app/(dashboard)/home/page.tsx
+++ frontend/src/app/(dashboard)/home/page.tsx
@@ -3,11 +3,37 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
-import { useState } from 'react';
-import { useQueries } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import Navbar from './Navbar';
 import Loading from '@/app/loading';
+import SelectMode from './SelectMode';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
+
+export interface newData {
+    rank: number;
+    username: string;
+    stats: {
+        rank: number;
+        league: string;
+    };
+}
+
+function ColorLeague(league: string) {
+    switch (league) {
+        case 'BRONZE':
+            return 'text-[#cd7f32]';
+        case 'SILVER':
+            return 'text-[silver]';
+        case 'GOLD':
+            return 'text-[gold]';
+        default:
+            return 'background';
+    }
+}
 
 export default function Page() {
+    const isMobile = useMediaQuery('(max-width: 768px)');
+
     const { data } = useQuery({
         queryKey: ['user'],
         queryFn: async () => {
@@ -21,96 +47,126 @@ export default function Page() {
         },
     });
 
-    // const { data: leaderboardData, isLoading } = useQuery({
-    //     queryKey: ['leaderboard'],
-    //     queryFn: async () => {
-    //         const { data } = await axios.get(`/api/leaderboard`, {
-    //             withCredentials: true,
-    //         });
-    //         return data;
-    //     },
-    // });
+    const { data: leaderboardData, isLoading } = useQuery({
+        queryKey: ['leaderboard'],
+        queryFn: async () => {
+            const { data } = await axios.get(`/api/leaderboard`, {
+                withCredentials: true,
+            });
+            return data;
+        },
+    });
+    if (isLoading) {
+        return <Loading />;
+    }
 
-    // if (isLoading) {
-    //     return <Loading />;
-    // }
 
-    // interface newData {
-    //     username: string;
-    //     stats: {
-    //         rank: number;
-    //         league: string;
-    //     };
-    // };
-    // const leaderboard = leaderboardData?.map(
-    //     (entry: newData) =>
-    //     ({
-    //         username: entry.username,
-    //         rank: entry.stats?.rank,
-    //         league: entry.stats?.league,
-    //     }) as { username: string; rank: number; league: string },
-    //     );
-    // const myleague = leaderboardData?.find((entry: newData) => entry.username === data?.data.username)?.stats?.league;
-    // console.log(myleague);
-    // return (
-    //     <div className="flex min-h-screen">
-    //       <nav className="flex flex-col justify-normal items-center bg-background text-black p-4 w-96 rounded-se-2xl drop-shadow-[0px_0px_2px_#ffffff] flex-1">
-    //         <p className="text-3xl font-jost text-white">
-    //             <img src={`/images/${myleague}-league.svg`} alt="" className='inline-block' />
-    //             &nbsp;  Leaderboard &nbsp;
-    //             <img src={`/images/${myleague}-league.svg`} alt="" className='inline-block' />
-    //             </p>
-    //         <hr className="border-t-2 border-gray-300 w-3/4 my-4" />
-    //         <div className="flex justify-center items-center">
-    //           {leaderboard.length > 0 && (
-    //             <table className="table-auto flex justify-center flex-col">
-    //               <thead>
-    //                 <tr className='px-4 py-2  rounded-lg'>
-    //                   <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Rank</th>
-    //                   <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Username</th>
-    //                   <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Score</th>
-    //                 </tr>
-    //               </thead>
-    //                 <hr className="flex justify-center items-center border-t-2 border-gray-400 w-4/5 rounded-xl m-auto" />
-    //               <tbody>
-    //                 {leaderboard.map((entry: newData, index) => (
-    //                   <tr key={entry.username} className={'flex justify-between bg-primary text-gray-50'}>
-    //                     <td className="px-4 py-2 whitespace-nowrap">{index + 1}</td>
-    //                     <td className="px-4 py-2 whitespace-nowrap">{entry.username}</td>
-    //                     <td className="px-4 py-2 whitespace-nowrap">{entry.rank}</td>
-    //                   </tr>
-    //                 ))}
-    //               </tbody>
-    //             </table>
-    //           )}
-    //         </div>
-    //       </nav>
-    //       <main className="w-full h-screen  m-auto flex justify-center  items-center text-black relative">
-    //         <div className=""></div>
-    //       </main>
-    //     </div>
-    //   );
-    return (
-        <main className="font-nicomoji text-5xl text-center py-10 text-secondary-200">
-            <h3>Welcome Back</h3>
-            <h2 className="font-jost text-opponent font-bold text-8xl my-6">
-                {data?.data.username}
-            </h2>
-            <h3>Select mode to start playing</h3>
-            <div className="my-8 lg:my-12 text-2xl flex justify-center gap-6 lg:gap-32">
-                <Link
-                    className="flex items-center justify-center w-44 h-12 border-[5px] rounded-3xl border-opponent bg-secondary-200 text-background"
-                    href="/game/normal"
+    const myleague = leaderboardData?.find(
+        (entry: newData) => entry.username === data?.data.username,
+    )?.stats?.league;
+    console.log(myleague);
+
+    function MyComponent() {
+        const leaderboard = useMemo(() => {
+            return leaderboardData?.map(
+                (entry: newData) =>
+                    ({
+                        username: entry.username,
+                        rank: entry.stats?.rank,
+                        league: entry.stats?.league,
+                    }) as { username: string; rank: number; league: string },
+            );
+        }, [leaderboardData]);
+
+        const myleague: string = useMemo(() => {
+            return leaderboardData?.find(
+                (entry: newData) => entry.username === data?.data.username,
+            )?.stats?.league;
+        }, [leaderboardData, data]);
+
+        console.log(leaderboard);
+
+        const Transation = isMobile === false ? '' : 'flex-col';
+
+        return (
+            <div className={`flex ${Transation}`}>
+                {useMemo(() => {
+                    return (
+                        <div className={`flex `}>
+                            {isMobile ? (
+                                <Link
+                                    href={'/leaderboard'}
+                                    className="h-12 w-12  mt-4"
+                                    title="Toggle leaderboard"
+                                    placeholder="Ranking"
+                                >
+                                    <img
+                                        src="/images/RankingStar.svg"
+                                        alt=""
+                                        className="w-full rounded-full bg-background  flex justify-center items-center shadow-lg drop-shadow-[0px_0px_10px_#FFE72D]"
+                                    />
+                                </Link>
+                            ) : (
+                                <>{Navbar(myleague, leaderboard)}</>
+                            )}
+                        </div>
+                    );
+                }, [leaderboard, myleague])}
+                <main
+                    className={`w-full min-h-[80vh] overflow-hidden flex flex-col justify-center items-center lg:justify-normal text-black transition-all duration-500 lg:my-16`}
                 >
-                    Normal
-                </Link>
-                <Link
-                    className="flex items-center justify-center w-44 h-12 border-[5px] rounded-3xl border-secondary-200 bg-opponent text-white"
-                    href="/game/ranked"
-                >
-                    Ranked
-                </Link>
+                    <div className=" text-white font-nicomoji text-2xl flex justify-center items-center w-[90%] justify-items-center bg-gradient-to-r from-background via-[#4B086D]  to-[#ACC0FE] bg-cover rounded-full border border-white p-2 lg:mx-10  lg:w-[600px] xl:[1000px]">
+                        <div className="flex-none relative w-20 h-20 mx-4 my-auto justify-self-start">
+                            <img
+                                src={data?.data.avatar}
+                                alt=""
+                                className="bg-primary rounded-full"
+                            />
+                            <div className="absolute -bottom-2 -right-1 w-8 shrink ">
+                                <div className="relative w-full flex flex-col justify-center items-center">
+                                    <img
+                                        src={`/images/star.svg`}
+                                        alt=""
+                                        className=""
+                                    />
+                                    <p className="absolute justify-center items-center text-black m-auto text-sm">
+                                        {
+                                            leaderboardData?.find(
+                                                (entry: newData) =>
+                                                    entry.username ===
+                                                    data?.data.username,
+                                            )?.stats?.level
+                                        }
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-span-2 grid grid-row-2 w-full">
+                            <h1 className="font-jost text-2xl text-white font-bold justify-items-start">
+                                {data?.data.username}
+                            </h1>
+                            <p
+                                className={`font-bold text-xl ${ColorLeague(
+                                    myleague,
+                                )} justify-items-start`}
+                            >{`${myleague}`}</p>
+                            <span className="w-[95%] rounded-full h-3 bg-white flex justify-start items-center relative group">
+                                <span className="lg:hidden absolute right-0 -top-5 h-4 px-1 rounded-full bg-slate-200 font-extralight text-[10px] text-center text-black group-hover:flex flex justify-center items-center">
+                                    500/1000
+                                </span>
+                                <span className="w-[50%] rounded-full h-1 bg-blue-500 ml-1"></span>
+                                {/* for level  */}
+                            </span>
+                        </div>
+                    </div>
+                    <div className="flex flex-col lg:flex-row lg:w-full justify-between items-stretch my-auto w-full">
+                       <SelectMode mode="Ranked" images="RankedGame" link="/game/Ranked" title="Ranked" />
+                        <SelectMode mode="Computer" images="Computer" link="/game/computer" title="computer" />
+                    </div>
+                </main>
             </div>
-        </main>
-    );
+        );
+    }
+
+    return <MyComponent />;
 }
