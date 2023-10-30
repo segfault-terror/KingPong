4f34afa4defa266eee8223cd8cf6710e20b56a3b
Hamza Haddani <hhamza@student.1337.ma>
2023-10-30 15:04:55 +0100

    chore: Add dummy users in database seed script

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,4 +1,5 @@
-import { AchievementType, NotifType, PrismaClient } from '@prisma/client';
+import { AchievementType, PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
@@ -24,31 +25,57 @@ async function seed() {
         skipDuplicates: true,
     });
 
-    const user = await prisma.user.create({
-        data: {
-            username: 'aaggoujj',
-            email: '',
-            password: '',
-            avatar: 'https://cdn.intra.42.fr/users/90acb3217b4be8350fa9f9fc32dd2200/aaggoujj.jpg',
-            fullname: 'Full Name',
-            stats:{
-                create: {
-                    wins: 0,
-                    losses: 0,
-                    rank: 1000,
-                    league: 'BRONZE'
-                }
-            }
-        },
-    });
-    await prisma.notification.createMany({
-        data: [
-            {
-                readed: false,
-                type: NotifType.GAME,
-                userId: user.id,
+    const users = [
+        {
+            fullname: 'Hamza Haddani',
+            username: 'Hamza',
+            email: 'h@g.com',
+            password: await bcrypt.hash('hamzahamza', 10),
+            avatar: 'https://cdn.intra.42.fr/users/40ff48680a112910914c132211cbc9a3/hhamza.jpg',
+            stats: {
+                wins: 0,
+                losses: 0,
+                rank: 1000,
+                league: 'BRONZE',
             },
-        ],
+        },
+        {
+            fullname: 'Aymane Aggoujjil',
+            username: 'Aymane',
+            email: 'a@g.com',
+            password: await bcrypt.hash('aymaneaymane', 10),
+            avatar: 'https://cdn.intra.42.fr/users/90acb3217b4be8350fa9f9fc32dd2200/aaggoujj.jpg',
+        },
+        {
+            fullname: 'Moussa Seddik',
+            username: 'Moussa',
+            email: 'm@g.com',
+            password: await bcrypt.hash('moussamoussa', 10),
+            avatar: 'https://cdn.intra.42.fr/users/811c6da3283271dcc1794c667938159c/moseddik.jpg',
+        },
+        {
+            fullname: 'Omar Aizab',
+            username: 'Omar',
+            email: 'o@g.com',
+            password: await bcrypt.hash('omaromar', 10),
+            avatar: 'https://cdn.intra.42.fr/users/10ebb5a3e3bf8fdf210566bfe0a102e0/oaizab.jpg',
+        },
+    ];
+
+    users.forEach(async (user) => {
+        await prisma.user.create({
+            data: {
+                ...user,
+                stats: {
+                    create: {
+                        wins: 0,
+                        losses: 0,
+                        rank: 1000,
+                        league: 'BRONZE',
+                    },
+                },
+            },
+        });
     });
 }
 
