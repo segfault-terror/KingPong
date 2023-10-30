da0f94ceca01521ad9d90032dc33377c8f7b3bdf
Hamza Haddani <hhamza@student.1337.ma>
2023-10-30 08:36:42 +0100

    feat(chat): Focus input when entering the chat page

--- frontend/src/app/chat/ChatInput.tsx
+++ frontend/src/app/chat/ChatInput.tsx
@@ -13,6 +13,7 @@ export default function ChatInput({ sendMessage }: ChatInputProps) {
             <input
                 ref={inputRef}
                 type="text"
+                autoFocus
                 placeholder="Write a message"
                 className="px-4 py-2 bg-background rounded-full
                     text-cube_palette-200 placeholder-cube_palette-200
