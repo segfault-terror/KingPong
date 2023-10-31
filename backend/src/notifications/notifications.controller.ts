5d4c55583d0d8e263823d9921c309b7687d79670
AymanAkashi <aaggoujj@student.1337.ma>
2023-10-31 17:26:42 +0100

    feat&fix(backend): add deleteAll& fix create notif

--- backend/src/notifications/notifications.controller.ts
+++ backend/src/notifications/notifications.controller.ts
@@ -1,33 +1,45 @@
-import { Body, Controller, Delete, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { AuthGard } from '../auth/auth.guard';
 import { use } from 'passport';
 
 @Controller('notifications')
+@UseGuards(AuthGard)
 export class NotificationsController {
     constructor(private readonly NotificationsService: NotificationsService) {}
 
     @Get()
-    @UseGuards(AuthGard)
     async getAll() {
         return this.NotificationsService.getAll();
     }
 
+    @Get("me")
+    async getmyAll(@Req() data: any) {
+        return this.NotificationsService.getmyAll(data.user.id);
+    }
+
     @Post('update')
-    @UseGuards(AuthGard)
     async update(@Body() data: any) {
         return this.NotificationsService.update(data.id, data);
     }
 
     @Delete('delete')
-    @UseGuards(AuthGard)
     async delete(@Body() data: any) {
         return this.NotificationsService.delete(data.id);
     }
 
+    @Delete('delete/all')
+    async deleteall(@Req() data: any) {
+        return this.NotificationsService.deleteAll(data.user.id);
+    }
+
     @Post('create')
-    @UseGuards(AuthGard)
     async create(@Body() data: any) {
         return this.NotificationsService.create(data);
+    }
+
+    @Get('notreaded')
+    async notreaded(@Req() data: any) {
+        return this.NotificationsService.notificationNotRead(data.user.id);
     }
 }
--- backend/src/notifications/notifications.service.ts
+++ backend/src/notifications/notifications.service.ts
@@ -1,18 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
+
 
 @Injectable()
 export class NotificationsService {
     constructor(private readonly prisma: PrismaService) {}
 
     async getAll() {
-        return this.prisma.notification.findMany(
-            {
-                include: {
-                    user: true,
-                }
-            }
-        );
+        return this.prisma.notification.findMany({
+            include: {
+                user: true,
+            },
+        });
+    }
+
+    async getmyAll(id: string) {
+        console.log("id: ",id);
+        return  await this.prisma.notification.findMany({
+            where: {
+                sendToId: id,
+            },
+            include: {
+                user: true,
+            },
+        });
     }
 
     async update(id: string, data: any) {
@@ -23,35 +34,62 @@ export class NotificationsService {
     }
 
     async delete(id: string) {
+        console.log(id);
         return this.prisma.notification.delete({
-            where: { id },
+            where: {
+                id,
+            },
+        });
+    }
+
+    async deleteAll(id: string) {
+        return await this.prisma.notification.deleteMany({
+            where: {
+                sendToId: id,
+            },
         });
     }
 
     async create(data: any) {
-
-        const notification = await this.prisma.notification.findMany({
+        const me = await this.prisma.user.findUnique({
             where: {
-                senderId: data.senderId,
-                userId: data.userId,
+                id: data.userId,
+            },
+            select:{
+                Notifications: true,
+                username: true,
             }
         });
 
-        if (!!notification.find((n)=> {
-            return n.type === data.type && n.senderId === data.senderId && n.userId === data.userId;
-        }))
-        return notification;
-        return this.prisma.notification.create({
+        if (!me) {
+            throw new NotFoundException('User not found');
+        }
+
+        if (me.Notifications.some((notification) => notification.sendToId === data.sendToId && notification.type === data.type)) {
+            throw new NotFoundException('Notification already exists');
+        }
+        const newNotif = await this.prisma.notification.create({
             data: {
-                senderId: data.senderId,
+                sendToId: data.sendToId,
                 type: data.type,
                 user: {
                     connect: {
-                        id: data.userId
-                    }
+                        id: data.userId,
+                    },
                 },
-            }
+            },
         });
+
+        newNotif.userId = data.userId;
+        return newNotif;
     }
 
+    async notificationNotRead(id: string) {
+        return await this.prisma.notification.findMany({
+            where: {
+                sendToId: id,
+                readed: false,
+            },
+        });
+    }
 }
