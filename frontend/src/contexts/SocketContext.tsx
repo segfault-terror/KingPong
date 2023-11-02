d0169f816774dba81156656587828d7f778aab71
Hamza Haddani <hhamza@student.1337.ma>
2023-11-02 20:58:05 +0100

    fix(chat): Socket connection on first time
    Fix socket connection for the first time by delaying it by a timeout of
    0ms

--- frontend/src/contexts/SocketContext.tsx
+++ frontend/src/contexts/SocketContext.tsx
@@ -13,15 +13,25 @@ export function useSocket() {
     return useContext(SocketContext);
 }
 
-export function SocketProvider(props: { children: ReactNode; username: string; }) {
+export function SocketProvider(props: {
+    children: ReactNode;
+    username: string;
+}) {
     const [socket, setSocket] = useState<Socket | null>(null);
-
 
     useEffect(() => {
         const newSocket = io('/chat', {
             withCredentials: true,
             path: '/api/socket',
+            autoConnect: false,
         });
+
+        async function connect() {
+            await new Promise((resolve) => setTimeout(resolve, 0));
+            newSocket.connect();
+        }
+
+        connect();
         newSocket.emit('register', props.username);
 
         setSocket(newSocket);
