83f3e07b2315275ef55db9d20936841b2ba2fb20
Omar Aizab <omaraizab98@gmail.com>
2023-11-02 16:02:07 +0100

    feat: create and move the paddles

--- frontend/src/app/(game)/game/computer/p5Matter.ts
+++ frontend/src/app/(game)/game/computer/p5Matter.ts
@@ -1,5 +1,5 @@
 import p5Types from 'p5';
-import { Engine, World, Runner, Bodies } from 'matter-js';
+import { Engine, World, Runner, Bodies, Body, Events } from 'matter-js';
 import { Box } from './Box';
 import { PongTable } from './PongTable';
 import { Ball } from './Ball';
@@ -8,6 +8,9 @@ let engine: Engine;
 export let world: World;
 let balls: Ball[] = [];
 let pongTable: PongTable;
+let topPaddle: Box;
+let bottomPaddle: Box;
+let ball: Ball;
 
 export function setup(p5: p5Types, canvasParentRef: Element) {
     p5.createCanvas(500, 800).parent(canvasParentRef);
@@ -16,6 +19,22 @@ export function setup(p5: p5Types, canvasParentRef: Element) {
     world = engine.world;
     Runner.run(engine);
     pongTable = new PongTable(p5.width, p5.height);
+    topPaddle = new Box(p5.width / 2, 50, 100, 20, { isStatic: true });
+    bottomPaddle = new Box(p5.width / 2, p5.height - 50, 100, 20, { isStatic: true });
+    ball = new Ball(p5.width / 2, p5.height / 2, 10, { friction: 0, frictionAir: 0, frictionStatic: 0, restitution: 1 });
+    Body.applyForce(ball.body, { x: ball.body.position.x, y: ball.body.position.y }, { x: 0, y: 0.0025 });
+
+    Events.on(engine, 'collisionStart', (event) => {
+        const pairs = event.pairs;
+        console.log(pairs);
+        for (let i = 0; i < pairs.length; i++) {
+            const pair = pairs[i];
+            if (pair.bodyA === ball.body) {
+                console.log('The ball has hit something!');
+                Body.applyForce(ball.body, { x: ball.body.position.x, y: ball.body.position.y }, { x: 0, y: 0.025 });
+            }
+        }
+    });
 }
 
 export function draw(p5: p5Types) {
@@ -26,11 +45,35 @@ export function draw(p5: p5Types) {
             balls[i].resetPosition(p5);
         }
     }
+    ball.show(p5);
+    if (ball.isOffScreen(p5)) {
+        ball.resetPosition(p5);
+    }
+    topPaddle.show(p5);
+    bottomPaddle.show(p5);
+    move(p5);
     // pongTable.show(p5);
 }
 
 export function mousePressed(p5: p5Types) {
-    balls.push(new Ball(p5.mouseX, p5.mouseY, 10, { friction: 0, frictionAir: 0, frictionStatic: 0, restitution: 1 }));
-    balls[balls.length - 1].body.force.x = p5.random(-0.05, 0.05);
-    balls[balls.length - 1].body.force.y = p5.random(-0.05, 0.05);
+    // balls.push(new Ball(p5.mouseX, p5.mouseY, 10, { friction: 0, frictionAir: 0, frictionStatic: 0, restitution: 1 }));
+    // balls[balls.length - 1].body.force.x = p5.random(-0.05, 0.05);
+    // balls[balls.length - 1].body.force.y = p5.random(-0.05, 0.05);
+}
+
+export function move(p5: p5Types) {
+    const LEFT_ARROW = 37;
+    const RIGHT_ARROW = 39;
+
+    if (p5.keyIsDown(LEFT_ARROW)) {
+        if (bottomPaddle.body.position.x < 50) {
+            return;
+        }
+        Body.setPosition(bottomPaddle.body, { x: bottomPaddle.body.position.x - 5, y: bottomPaddle.body.position.y });
+    } else if (p5.keyIsDown(RIGHT_ARROW)) {
+        if (bottomPaddle.body.position.x > p5.width - 50) {
+            return;
+        }
+        Body.setPosition(bottomPaddle.body, { x: bottomPaddle.body.position.x + 5, y: bottomPaddle.body.position.y });
+    }
 }
--- frontend/src/app/(game)/game/computer/page.tsx
+++ frontend/src/app/(game)/game/computer/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-import { setup, draw, mousePressed } from './p5Matter';
+import { setup, draw, mousePressed, keyPressed } from './p5Matter';
 
 const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
     ssr: false,
@@ -11,7 +11,12 @@ const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
 export default function Page() {
     return (
         <div className="flex justify-center items-center h-screen">
-            <Sketch className={'border border-red-700 rounded-3xl overflow-hidden'} setup={setup} draw={draw} mousePressed={mousePressed} />
+            <Sketch
+                className={'border border-red-700 rounded-3xl overflow-hidden'}
+                setup={setup}
+                draw={draw}
+                mousePressed={mousePressed}
+            />
         </div>
     );
 }
