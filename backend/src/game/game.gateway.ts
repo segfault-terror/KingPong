42881f7946b04a383d8aaa3cccb20ee3e68639c2
AymanAkashi <aaggoujj@student.1337.ma>
2023-11-10 21:54:27 +0100

    feat: add matchmaking with two players

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,20 +1,27 @@
 import {
+    ConnectedSocket,
     MessageBody,
     OnGatewayConnection,
     SubscribeMessage,
     WebSocketGateway,
+    WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Socket, Namespace } from 'socket.io';
 import { ComputerService } from './computer/computer.service';
 import { GameService } from './game.service';
+import { ConsoleLogger } from '@nestjs/common';
 
 @WebSocketGateway({
     namespace: 'game',
 })
 export class GameGateway implements OnGatewayConnection {
-    constructor(private readonly computerService: ComputerService, private readonly gameService: GameService) {}
-    connectedUsers: { id: string; sockets: string }[] = [];
-    queue: { league: string; socket: string }[] = [];
+    constructor(
+        private readonly computerService: ComputerService,
+        private readonly gameService: GameService,
+    ) {}
+    @WebSocketServer() server: Namespace;
+    connectedUsers: { id: string; username: string; sockets: string }[] = [];
+    queue: { username: string; league: string; socket: string }[] = [];
 
     @SubscribeMessage('message')
     handleMessage(client: Socket, payload: any): string {
@@ -26,34 +33,92 @@ export class GameGateway implements OnGatewayConnection {
     handleConnection(client: Socket) {
         const user = this.connectedUsers.find((user) => user.id === client.id);
 
-        if (!user)
+        if (!user) {
+            console.log('new client', client.id);
             this.connectedUsers.push({
                 id: client.id,
                 sockets: client.id,
+                username: '',
             });
-        else client.disconnect(true);
+        }
+        // else client.disconnect(true);
 
         console.log('client', client.id);
         this.computerService.startGame(client);
     }
 
+    async handleDisconnect(socket: Socket) {
+        const user = this.connectedUsers.find((user) =>
+            user.sockets.includes(socket.id),
+        );
+
+        if (!user) return;
+        console.log(`[Global] Unregistered ${user.username} from one tab`);
+    }
+
+    @SubscribeMessage('register')
+    async handleRegister(
+        @MessageBody() username: string,
+        @ConnectedSocket() socket: Socket,
+    ) {
+        console.log(`Register... ${username}`);
+        const user = this.connectedUsers.find(
+            (user) => user.username === username,
+        );
+        if (!user) {
+            console.log(`[Global] Registered ${username} for the first time`);
+            this.connectedUsers.push({
+                username,
+                sockets: socket.id,
+                id: socket.id,
+            });
+        } else {
+            // disallow multiple connections
+            if (user.sockets !== socket.id) {
+                console.log(`[Global] ${username} already has a connection`);
+                socket.disconnect(true);
+            }
+        }
+    }
+
     @SubscribeMessage('matchmaking')
-    handleGame(@MessageBody() data: any) {
-        console.log('matchmaking', data);
-        // const user = this.connectedUsers.find((user) => user.id === data.id);
-        // if (!user) return;
-        // console.log('matchmaking', data);
-        // const league = data.stats.league;
-        // //find user socket
-        // this.queue.push({ league, socket: user.sockets });
-
-        // // Filter the queue based on the length of the league
-        // const filteredQueue = this.queue.filter((player) => player.league === league);
-
-        // if (filteredQueue.length >= 2) {
-        //     const player1 = filteredQueue.shift();
-        //     const player2 = filteredQueue.shift();
-        //     this.gameService.startGame(player1, player2);
-        // }
+    async handleGame(@MessageBody() data: any) {
+        const user = this.connectedUsers.find(
+            (user) => user.username === data.username,
+        );
+        if (!user) return;
+        const UserQueue = this.queue.find(
+            (queue) => queue.username === data.username,
+        );
+        if (!UserQueue) {
+            this.queue.push({
+                username: data.username,
+                league: data.league,
+                socket: user.sockets,
+            });
+            //filter queue by league if possible to match 2 players in the seem league
+            const queue = this.queue.filter(
+                (queue) => queue.league === data.league,
+            );
+            if (queue.length >= 2) {
+                console.log('matchmaking', '2 players found');
+                this.server.to(queue[0].socket).emit('matchmakingfound', true);
+                this.server.to(queue[1].socket).emit('matchmakingfound', true);
+                // const player1 = queue[0];
+                // const player2 = queue[1];
+                // this.gameService.createGame(
+                //     player1.username,
+                //     player2.username,
+                //     player1.socket,
+                //     player2.socket,
+                // );
+                // this.queue = this.queue.filter(
+                //     (queue) =>
+                //         queue.username !== player1.username &&
+                //         queue.username !== player2.username,
+                // );
+            }
+            // console.log('matchmaking', data);    
+        }
     }
 }
