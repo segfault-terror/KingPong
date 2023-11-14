ef4bfb45f38015d92acafa32985736c53c0be47b
AymanAkashi <aaggoujj@student.1337.ma>
2023-11-14 21:50:59 +0100

    feat(backend): add match to game table

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -1,12 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable, Param } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class GameService {
     constructor(private readonly prisma: PrismaService) {}
 
-	async startGame(player1: any, player2: any) {
-		console.log('player1', player1);
-		console.log('player2', player2);
-	}
+    async AddMatch(
+        player1: string,
+        player2: string,
+        ranked: boolean,
+        player1_score: number,
+        player2_score: number,
+    ) {
+        const user1 = await this.prisma.user.findUnique({
+            where: {
+                username: player1,
+            },
+        });
+        const user2 = await this.prisma.user.findUnique({
+            where: {
+                username: player2,
+            },
+        });
+        if (!user1 || !user2) {
+            return null;
+        }
+        const match = await this.prisma.game.create({
+            data: {
+                player1: {
+                    connect: {
+                        id: user1.id,
+                    },
+                },
+                player2: {
+                    connect: {
+                        id: user2.id,
+                    },
+                },
+                ranked: ranked,
+                player1_score: player1_score,
+                player2_score: player2_score,
+            },
+        });
+        return match;
+    }
+
+    
+    async getLastMatch(@Param('username') username: string) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                username,
+            },
+        });
+
+		if (!user) {
+			return null;
+		}
+		
+		const match = await this.prisma.game.findFirst({
+			where: {
+				OR: [
+					{
+						player1: {
+							username,
+						},
+					},
+					{
+						player2: {
+							username,
+						},
+					},
+				],
+			},
+			orderBy: {
+				createdAt: 'desc',
+			},
+		});
+		return match;
+    }
 }
