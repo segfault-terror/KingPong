c4476e71bed18375c7a4deb87c3e729edbe9afe6
AymanAkashi <aaggoujj@student.1337.ma>
2023-11-23 22:15:22 +0100

    feat(db): create achiviement in db

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -4,25 +4,115 @@ import * as bcrypt from 'bcryptjs';
 const prisma = new PrismaClient();
 
 async function seed() {
+   
     await prisma.achievement.createMany({
         data: [
             {
-                title: 'Pong Pro',
-                description: 'Win 10 game of pong',
+                title: 'Fast Pong Bronze',
+                description: 'Win a round in less than 20 seconds',
                 type: AchievementType.BRONZE,
             },
             {
-                title: 'Pong Veteran',
-                description: 'Win 50 games of pong',
+                title: 'Fast Pong Silver',
+                description: 'Win a round in less than 15 seconds',
                 type: AchievementType.SILVER,
             },
             {
-                title: 'Pong King',
-                description: 'Win 100 games of pong',
+                title: 'Fast Pong Gold',
+                description: 'Win a round in less than 7 seconds',
+                type: AchievementType.GOLD,
+            },
+            {
+                title: 'Easy Striker Bronze',
+                description: 'Win a Match with 1 goals difference',
+                type: AchievementType.BRONZE,
+            },
+            {
+                title: 'Easy Striker Silver',
+                description: 'Win a Match with 2 goals difference',
+                type: AchievementType.SILVER,
+            },
+            {
+                title: 'Easy Striker Gold',
+                description: 'Win a Match with 7 goals difference',
+                type: AchievementType.GOLD,
+            },
+            {
+                title: 'Long Touch Paddle Bronze',
+                description: 'Win a Match with 30 touches',
+                type: AchievementType.BRONZE,
+            },
+            {
+                title: 'Long Touch Paddle Silver',
+                description: 'Win a Match with 50 touches',
+                type: AchievementType.SILVER,
+            },
+            {
+                title: 'Long Touch Paddle Gold',
+                description: 'Win a Match with 70 touches',
+                type: AchievementType.GOLD,
+            },
+            {
+                title: 'Long Time Round Bronze',
+                description: 'Scored a goal after 30 seconds',
+                type: AchievementType.BRONZE,
+            },
+            {
+                title: 'Long Time Round Silver',
+                description: 'Scored a goal after 20 seconds',
+                type: AchievementType.SILVER,
+            },
+            {
+                title: 'Long Time Round Gold',
+                description: 'Scored a goal after 10 seconds',
+                type: AchievementType.GOLD,
+            },
+            {
+                title: 'Winning Streak Bronze',
+                description: 'Win 3 matches in a row',
+                type: AchievementType.BRONZE,
+            },
+            {
+                title: 'Winning Streak Silver',
+                description: 'Win 5 matches in a row',
+                type: AchievementType.SILVER,
+            },
+            {
+                title: 'Winning Streak Gold',
+                description: 'Win 10 matches in a row',
+                type: AchievementType.GOLD,
+            },
+            {
+                title: 'Clean Sheet Bronze',
+                description: 'Win a match without conceding a goal',
+                type: AchievementType.BRONZE,
+            },
+            {
+                title: 'Clean Sheet Silver',
+                description: 'Win 3 matches without conceding a goal',
+                type: AchievementType.SILVER,
+            },
+            {
+                title: 'Clean Sheet Gold',
+                description: 'Win 5 matches without conceding a goal',
+                type: AchievementType.GOLD,
+            },
+            {
+                title: 'Best of all Bronze',
+                description: 'Won all achievements bronze',
+                type: AchievementType.BRONZE,
+            },
+            {
+                title: 'Best of all Silver',
+                description: 'Won all achievements silver',
+                type: AchievementType.SILVER,
+            },
+            {
+                title: 'Best of all Gold',
+                description: 'Won all achievements gold',
                 type: AchievementType.GOLD,
             },
         ],
-        skipDuplicates: true,
     });
 
     const users = [
