7a029ae35d4fd2072dbb7a89a003038723767908
AymanAkashi <aaggoujj@student.1337.ma>
2023-11-24 16:45:27 +0100

    style(frontend): change the style of signing page

--- frontend/src/app/(authentication)/signup/page.tsx
+++ frontend/src/app/(authentication)/signup/page.tsx
@@ -8,7 +8,7 @@ export default function SignUpPage() {
                 className="w-full h-20 flex items-center justify-start gap-2
                         font-jost font-bold text-2xl text-white"
             >
-                <Link className="text-inactive-200" href="/signin">
+                <Link className="text-inactive-200 hover:scale-105 hover:text-gray-400 transition-all duration-100 ease-in-out delay-75" href="/signin">
                     Sign in
                 </Link>
                 <span className="text-inactive-500"> / </span>
